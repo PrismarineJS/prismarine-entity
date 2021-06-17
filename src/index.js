ba2986ae25c0209f3f30e7af429e39230d26315a
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-06-18 00:38:17 +0300

    PrismarineEntity 2.0!

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 const Vec3 = require('vec3').Vec3
+const EventEmitter = require('events').EventEmitter
 
-module.exports = class Entity extends EventEmitter {
-  constructor(id) {
-    this.id = id
-    this.type = null
-    this.position = new Vec3(0, 0, 0)
-    this.velocity = new Vec3(0, 0, 0)
-    this.yaw = 0
-    this.pitch = 0
-    this.onGround = true
-    this.height = 0
-    this.width = 0
-    this.effects = {}
-    // 0 = held item, 1-4 = armor slot
-    this.equipment = new Array(5)
-    this.heldItem = this.equipment[0] // shortcut to equipment[0]
-    this.isValid = true
-    this.metadata = []
+module.exports = function (version) {
+  class Entity extends EventEmitter {
+    constructor (id, metadataReadingEnabled) {
+      super()
+      this.id = id ?? 0
+      this.version = version ?? '1.12.2'
+      this.metadataReading = metadataReadingEnabled ?? false
+      this.type = null
+      this.position = new Vec3(0, 0, 0)
+      this.velocity = new Vec3(0, 0, 0)
+      this.yaw = 0
+      this.pitch = 0
+      this.onGround = true
+      this.height = 0
+      this.width = 0
+      this.effects = {}
+      // 0 = Held item, 1-4 = armor slot
+      this.equipment = new Array(5)
+      this.heldItem = this.equipment[0] // Shortcut to equipment[0]
+      this.isValid = true
+      this.metadata = []
+
+      if (this.metadataReading === true) {
+        const mcData = require('minecraft-data')(version ?? '1.12.2')
+
+        this.isOnFire = () => Boolean(this.metadata[0] & 0x01)
+        this.isCrouched = () => Boolean(this.metadata[0] & 0x02)
+        this.isSprinting = () => Boolean(this.metadata[0] & 0x08)
+        this.isInvisible = () => Boolean(this.metadata[0] & 0x20)
+
+        if (mcData.isOlderThan('1.9')) {
+          this.isMovingSlowly = () => Boolean(this.metadata[0] & 0x10) // Moving slowly, ie: Eating/Drinking/Blocking
+        } else if (mcData.isNewerOrEqualTo('1.9')) {
+          this.isSwimming = () => Boolean(this.metadata[0] & 0x10)
+          this.isGlowing = () => Boolean(this.metadata[0] & 0x40) // Has glowing effect
+          this.isFlyingElytra = () => Boolean(this.metadata[0] & 0x80)
+        }
+      }
+    }
+
+    setEquipment (index, item) {
+      this.equipment[index] = item
+      this.heldItem = this.equipment[0]
+    }
   }
 
-  setEquipment(index, item) {
-    this.equipment[index] = item
-    this.heldItem = this.equipment[0]
-  }
+  return Entity
 }
--- test/internalTest.js
+++ test/internalTest.js
@@ -1,10 +1,43 @@
 /* eslint-env mocha */
 
-const prismarineEntity = require('../')
-const versions = reqiure('../src/version')
+const versions = require('../src/version')
 
 for (const supportedVersion of versions.testedVersions) {
-    describe(`prismarineEntity ${supportedVersion}`, function () {
-      let entity = new prismarineEntity(0, '1.12.2')
-    }
+  describe(`prismarineEntity ${supportedVersion}`, function () {
+    const PrismarineEntity = require('../')(supportedVersion)
+
+    it('creates an valid entity', (done) => {
+      let Entity = new PrismarineEntity(0, false)
+      if (Entity.isValid === true) {
+        done()
+      } else {
+        throw new Error('Entity is not valid!?')
+      }
+    })
+    it('reads metadata', (done) => {
+      let Entity = new PrismarineEntity(0, true)
+      if (Entity.isValid === true) {
+        if (Entity.isOnFire() === false) {
+          done()
+        } else {
+          throw new Error('Entity defaultly doesnt on fire!?')
+        }
+      } else {
+        throw new Error('Entity is not valid!?')
+      }
+    })
+    it('sets equipment', (done) => {
+      let Entity = new PrismarineEntity(0, false)
+      if (Entity.isValid === true) {
+        Entity.setEquipment(0, 'fakeItem')
+        if (Entity.heldItem == 'fakeItem') {
+          done()
+        } else {
+          throw new Error('Cannot set equipment!?')
+        }
+      } else {
+        throw new Error('Entity is not valid!?')
+      }
+    })
+  })
 }
