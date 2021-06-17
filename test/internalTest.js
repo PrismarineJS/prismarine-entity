25d682593a0d457208c76b43d7ee8863433c928d
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-06-18 00:00:28 +0300

    Update internalTest.js

--- test/internalTest.js
+++ test/internalTest.js
@@ -1,4 +1,10 @@
 /* eslint-env mocha */
 
 const prismarineEntity = require('../')
+const versions = reqiure('../src/version')
 
+for (const supportedVersion of versions.testedVersions) {
+    describe(`prismarineEntity ${supportedVersion}`, function () {
+      let entity = new prismarineEntity(0, '1.12.2')
+    }
+}
